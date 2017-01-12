b4044ac02c6bdd50c124356ff48614033aa7e9e8
Paul <paultk@stud.ntnu.no>
2017-01-12 13:01:03 +0100

    !

--- app/app.component.ts
+++ app/app.component.ts
@@ -2,6 +2,9 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'my-app',
-  template: `<h1>Hello {{name}}</h1>`,
+  template: `<h1>Hello {{name}}</h1>
+ <my-add-user></my-add-user>
+`,
+
 })
 export class AppComponent  { name = 'Angular'; }
--- app/app.module.ts
+++ app/app.module.ts
@@ -1,11 +1,24 @@
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
 
 import { AppComponent }  from './app.component';
+import { AddUserComponent } from './add-user.component';
 
 @NgModule({
-  imports:      [ BrowserModule ],
-  declarations: [ AppComponent ],
+  imports:      [
+    BrowserModule,
+    FormsModule,
+    ReactiveFormsModule,
+    HttpModule
+
+  ],
+  declarations: [
+    AppComponent,
+    AddUserComponent,
+  ],
+  providers: [],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
