8b617189b4a20d3d88d0e25b47f4338763160fc9
Håkon <haakbe@student.hist.no>
2017-01-23 13:39:08 +0100

    desgin faq

--- app/app.component.ts
+++ app/app.component.ts
@@ -3,8 +3,13 @@ import {Component} from '@angular/core';
 @Component({
   selector: 'my-app',
   template: `
+    <!--<my-navigation></my-navigation>-->
+    <!--<my-userinfo></my-userinfo>-->
+    <!--<my-profil></my-profil>
     <!--<my-login></my-login>-->
-    <my-navigation></my-navigation>
+    <!--<user-form></user-form>-->
+    <!--<my-fravaer></my-fravaer>-->
+    <faq></faq>
   `,
 
 })
