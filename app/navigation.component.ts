f3c69b948fac07900a430d4dee8542d5b8c400bf
Quinky <jenskaarud@gmail.com>
2017-01-25 13:44:47 +0100

    typescript for å hente uleste meldinger

--- app/navigation.component.ts
+++ app/navigation.component.ts
@@ -6,6 +6,7 @@ import {Component, OnInit} from "@angular/core";
 
 import {User} from './user';
 import {UserService} from './user.service';
+import {Http, Headers} from "@angular/http";
 
 @Component({
   moduleId: module.id,
@@ -17,17 +18,37 @@ import {UserService} from './user.service';
 export class NavigationComponent implements OnInit{
   users: User[];
   selectedUser: User;
+  numMessages: number;
+
+  private headers = new Headers({'Content-Type': 'application/json'});
+
 
   constructor (
-    private userService: UserService
-  ) {}
+    private userService: UserService,
+    private http: Http,
+) {}
 
   getUsers(): void {
     this.userService.getUsers().then(users => this.users = users);
   }
 
+  setNumMessages(): void {
+    this.selectedUser = this.userService.getCurrentUser();
+    const URL = 'http://localhost:8080/melding/get/ulest/ant';
+    console.log("from navigation.component.setNumMessages()");
+    this.http.post(URL, JSON.stringify(this.selectedUser), {headers: this.headers},)
+      .toPromise()
+      .then((res) => {
+        this.numMessages = parseInt(res.text());
+        console.log(this.numMessages);})
+      .catch((res) => {
+        console.log(res);
+    })
+  }
+
   ngOnInit(): void {
     this.getUsers();
     this.selectedUser = this.userService.getCurrentUser();
+    this.setNumMessages();
   }
 }
