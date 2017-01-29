d2b893b8973b339b02482c4e139464a897d04206
Paul <paultk@stud.ntnu.no>
2017-01-29 21:53:06 +0100

    d

--- app/_services/shift.service.ts
+++ app/_services/shift.service.ts
@@ -17,7 +17,7 @@ export class ShiftService {
   constructor(
     private http: Http
   ){}
-  private headers = new Headers({'Content-Type': 'application/json'});
+  private headers = new Headers({'Content-Type': 'application/json', 'token': localStorage.getItem('sessionToken')});
   private handleError(error: any): Promise<any> {
     console.error('An error occured', error);
     return Promise.reject(error.message || error);
@@ -28,7 +28,7 @@ export class ShiftService {
     let url = `http://localhost:8080/vakt/all/month/${date.toISOString().substr(0, 7)}-01T12:00:00+01:00/2`;
     let time = new Date();
     console.log('first' + time);
-    return this.http.get(url).map(
+    return this.http.get(url, {headers : this.headers}).map(
       (response: Response) => response.json()
     );
   }
@@ -64,22 +64,35 @@ export class ShiftService {
     else if(tempTid == 0) date += '23:00';
 
     let url = `http://localhost:8080/bruker/addtid/${date}/2`;
-    console.log("user:");
-    console.log(JSON.stringify(shift.user));
-    /*
-     this.http.post(url, JSON.stringify(shift.user), {headers: this.headers}).toPromise()
-     .then((response) => console.log(response))
-     .catch( (error) => this.handleError(error));*/
+    this.http.post(url, JSON.stringify(shift.user), {headers: this.headers}).toPromise()
+      .then((response) => console.log(response))
+      .catch( (error) => this.handleError(error));
   }
 
-  //todo: urgent fix on how date in url gets managed
   getAvailables(date: Date) {
     let dateTo = date.toISOString().substr(0, 7);
-    let url = `http://localhost:8080/tilgjengelighet/all/${dateTo}-01T00:00:00/${dateTo}-28T00:00:00`;
+    let daysInMonthString = this.daysInMonth(date);
+    let url = `http://localhost:8080/tilgjengelighet/all/${dateTo}-01T00:00:00/${dateTo}-${daysInMonthString}T00:00:00`;
     console.log(url);
-    return this.http.get(url).map(
+    return this.http.get(url, {headers : this.headers}).map(
       (response: Response) => response.json()
     );
+  }
+
+  getVaktBytter() {
+    let url = `http://localhost:8080/vaktbytte/get/alle`;
+    console.log(url);
+    return this.http.get(url, {headers : this.headers}).map(
+      (response: Response) => response.json()
+    );
+  }
+  addVaktBytte(vaktBytte: any): Promise<boolean> {
+
+
+    let url = 'http://localhost:8080/vaktbytte/bytt';
+    return this.http.post(url, JSON.stringify(vaktBytte), {headers: this.headers}).toPromise()
+      .then((response) => response.json())
+      .catch( (error) => this.handleError(error));
   }
 
   addAvailability(availability: any): Promise<boolean> {
@@ -95,5 +108,27 @@ export class ShiftService {
 
   changeShifts(): void {
 
+  }
+
+  daysInMonth(date: Date):string {
+    let daysInMonth = new Date(date.getFullYear(), date.getMonth(), 0).getDate();
+    let returnString = '';
+    if (daysInMonth < 10) returnString += '0';
+    return (returnString += daysInMonth);
+
+  }
+
+  byttVakter(vaktBytte: any) {
+    let url = 'http://localhost:8080/brukervakt/bytt';
+    this.http.post(url, JSON.stringify(vaktBytte), {headers: this.headers}).toPromise()
+      .then((response) => response.json())
+      .catch( (error) => this.handleError(error));
+  }
+
+  deleteVaktBytte(vaktBytte: any): void {
+    let url = 'http://localhost:8080/vaktbytte/delete';
+    this.http.post(url, JSON.stringify(vaktBytte), {headers: this.headers}).toPromise()
+      .then((response) => response.json())
+      .catch( (error) => this.handleError(error));
   }
 }
---
+++ app/vakt_bytte/vakt-bytte.component.ts
@@ -0,0 +1,144 @@
+import {Component, OnInit} from '@angular/core';
+
+import { Fravaer } from '../_models/fravaer';
+import {Vakt} from '../_models/vakt';
+import {FravaerService} from "../_services/fravaer.service";
+import {User} from "../_models/user";
+import {Shift} from "../_models/shift";
+import { UserService } from "../_services/user.service";
+import { ShiftService } from "../_services/shift.service";
+
+@Component({
+  moduleId: module.id,
+  selector: 'vakt-bytte',
+  templateUrl: 'vakt-bytte.component.html',
+  styleUrls: [ 'vakt-bytte.component.css' ]
+})
+
+export class VaktBytteComponent implements OnInit{
+  constructor(
+    private userService: UserService,
+    private shiftService: ShiftService,
+  ) {}
+
+  brukerId2 = 0;
+
+  months = ['Januar', 'Februar', 'Mars', 'April', 'Mai', 'Juni', 'Juli', 'August', 'September', 'Oktober', 'November', 'Desember'];
+  date: Date;
+  allUsers: User[];
+  usersIndexed: User[] = [];
+  monthShifts: any[] = [];
+  daysShifts: Shift[][] = [];
+  vaktBytter: any[];
+  shiftsIndexedById: Shift[] = [];
+
+  vaktInForm: User;
+
+  ngOnInit(): void {
+    this.date = new Date();
+
+    this.userService.getUsers1()
+      .subscribe(
+        (observable) => this.restOfInit(observable)
+      );
+
+
+  }
+
+  getVaktBytter(): void {
+    this.shiftService.getVaktBytter().subscribe(
+      response => this.vaktBytter = response
+    )
+  }
+
+  getShifts(): void {
+    this.shiftService.getShifts(this.date).subscribe(
+      (val) => this.setShifts(val)
+    );
+
+  }
+
+  restOfInit(users: User[]): void {
+    this.allUsers = users.map(user =>  new User(user['brukerId'], null, user['stillingsBeskrivelse'], null, user['stillingsProsent'],
+      null, null, user['fornavn'], user['etternavn'], user['epost'], user['avdelingId']
+    ));
+
+    this.getShifts();
+  }
+
+  //todo: minimalize shift
+  setShifts(shifts: any[]): void {
+
+    console.log('here   :');
+    console.log(shifts);
+    this.monthShifts = shifts;
+
+    for (let i = 0; i < 32; i++) {
+      this.daysShifts[i] = [];
+    }
+    for (let i in this.monthShifts) {
+      let date = new Date(this.monthShifts[i]['vakt']['tilTid']);
+      let vaktAnsvarligId = this.monthShifts[i]['vakt']['vaktansvarligId'];
+      // night = toTime = 0700, day = toTime = 1500, evening = toTime = 2300
+      let toTimeSliced = this.monthShifts[i]['vakt']['tilTid'].slice(11, 13) as number;
+      let fromTime = this.monthShifts[i]['vakt']['tilTid'].slice(0, 16) as number;
+      let shiftDescript = 7;
+      if (toTimeSliced > 7 && toTimeSliced < 1500) shiftDescript = 15;
+      else if (toTimeSliced > 1500) shiftDescript = 23;
+
+      //todo: extremely hacky, should be fixed
+
+
+      let brukerId = (this.monthShifts[i]['brukerId'] == 0) ? vaktAnsvarligId : this.monthShifts[i]['brukerId'];
+      let vaktId = this.monthShifts[i]['vakt']['vaktId'];
+
+      let tempShift = new Shift(null, fromTime, shiftDescript, brukerId, vaktAnsvarligId,  vaktId);
+
+      this.shiftsIndexedById[tempShift.vaktId] = tempShift;
+      this.daysShifts[date.getDate()].push(
+        tempShift);
+
+
+    }
+
+
+
+    let daysInMonth = new Date(this.date.getFullYear(), this.date.getMonth(), 0).getDate();
+
+    this.allUsers.forEach(
+      user =>
+        this.usersIndexed[user.brukerId] = user
+    );
+    this.monthShifts.forEach(
+      shift => this.shiftsIndexedById[shift.vaktId] = shift
+    );
+
+    //this.vaktInForm = this.allUsers[0];
+
+    this.getVaktBytter();
+
+    this.brukerId2 = this.allUsers[0].brukerId;
+
+  }
+
+
+  godkjennVaktBytte(vaktBytte: any): void {
+    if(vaktBytte['brukerId2'] == 0) vaktBytte['brukerId2'] = this.brukerId2;
+    this.shiftService.byttVakter(vaktBytte);
+
+  }
+
+  slettVaktBytte(vaktBytte: any): void {
+    this.shiftService.deleteVaktBytte(vaktBytte);
+
+    for(let i in this.vaktBytter) {
+      if(this.vaktBytter[i] == vaktBytte) {
+        this.vaktBytter.splice( Number(i) , 1);
+        break;
+      }
+    }
+  }
+  check(): void {
+  }
+
+}
